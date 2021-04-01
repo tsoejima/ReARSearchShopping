2b9e17f81850c68a107232215182be6b8a92c7dc
tsoejima <t.soejimadeveloper@gmail.com>
2021-04-01 16:11:07 +0900

    Initial Commit

---
+++ ReARSearchShopping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReARSearchShopping
+//
+//  Created by 副島拓哉 on 2021/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
